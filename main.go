1bd9a7a03ac1783844482aa63d76cbe5402d7894
Eric Miller <eric@legoaces.org>
2015-12-29 22:30:48 -0500

    Added code for Serial communications, as yet untested.

--- main.go
+++ main.go
@@ -1,26 +1,64 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/skratchdot/open-golang/open" // Opens file in external editor
+	"github.com/tarm/serial"
 	"os"
+	"time"
 )
 
+var port = flag.String("port", "", "What port is the Arduino connected to?")
+
 const (
-	FILENAME = "output.tsv"
+	FILENAME = "output.txt"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	flag.Parse()
+	if *port == "" {
+		fmt.Println("Port is required")
+		return
+	}
+	findArduino()
 	export_data("Hello World")
+}
+
+func findArduino() {
+	config := &serial.Config{Name: *port, Baud: 115200,
+		ReadTimeout: time.Second * 5}
+
+	s, err := serial.OpenPort(config)
+
+	if err != nil {
+		fmt.Printf("Unable to open serial port: %s", err.Error())
+		return
+	}
+
+	buf := make([]byte, 10000)
+	n, err := s.Read(buf)
+
+	fmt.Printf("Bytes recieved: %d", n)
+
+	if err != nil {
+		fmt.Printf("Unable to read serial port: %s", err.Error())
+		return
+	}
+	if n == 0 {
+		fmt.Printf("No data recieved")
+		return
+	}
+
+	fmt.Printf("Data read: \n\n%s\n\n", string(buf[:n]))
 }
 
 func export_data(data string) {
 	file, err := os.Create(FILENAME)
 
-	fmt.Printf("File: %s", file.Name())
 	if err != nil {
-		fmt.Errorf("Error opening file: %s", err.Error())
+		fmt.Printf("Error opening file: %s", err.Error())
+		return
 	}
 
 	file.WriteString(data)
