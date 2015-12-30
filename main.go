c60d9ac6daeeed6a4b258cc00872477d8aec85cb
Eric Miller <eric@legoaces.org>
2015-12-29 21:58:09 -0500

    Added basic code to write file and open in system viewer.

--- main.go
+++ main.go
@@ -2,8 +2,28 @@ package main
 
 import (
 	"fmt"
+	"github.com/skratchdot/open-golang/open" // Opens file in external editor
+	"os"
+)
+
+const (
+	FILENAME = "output.tsv"
 )
 
 func main() {
 	fmt.Println("Hello World")
+	export_data("Hello World")
+}
+
+func export_data(data string) {
+	file, err := os.Create(FILENAME)
+
+	fmt.Printf("File: %s", file.Name())
+	if err != nil {
+		fmt.Errorf("Error opening file: %s", err.Error())
+	}
+
+	file.WriteString(data)
+	file.Close()
+	open.Run(FILENAME)
 }
