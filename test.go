cf007f4a7c9dd85794b33738d97033d7e14df438
Eric Miller <eric@legoaces.org>
2015-12-30 21:45:14 -0500

    Added bug serial reading

--- test.go
+++ test.go
@@ -19,4 +19,27 @@ func main() {
 	for _, port := range ports {
 		fmt.Printf("Found port: %v\n", port)
 	}
+
+	port := ports[0]
+	recieveComms(port)
+}
+
+func recieveComms(port string) {
+
+	mode := &serial.Mode{
+		BaudRate: 9600,
+	}
+	sp, err := serial.OpenPort(port, mode)
+	defer sp.Close()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	buff := make([]byte, 100)
+	n, err := sp.Read(buff)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Println(string(buff[:n]))
 }
