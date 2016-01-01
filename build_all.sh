3933513da09bc814d32c28451b1c0b4b64476a9f
Eric Miller <eric@legoaces.org>
2016-01-01 12:40:39 -0500

    Altered build targets to include on 32 bit architectures.

--- build_all.sh
+++ build_all.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
 echo "Windows 32 bit"
-env GOOS=windows GOARCH=386 go build -v -o bin/temperature_logger_x32.exe
-echo "Windows 64 bit"
-env GOOS=windows GOARCH=amd64 go build -v -o bin/temperature_logger_x64.exe
+env GOOS=windows GOARCH=386 go build -v -o bin/temperature_logger-windows.exe
+#echo "Windows 64 bit"
+#env GOOS=windows GOARCH=amd64 go build -v -o bin/temperature_logger_x64.exe
 
 echo "Linux 32 bit"
-env GOOS=linux GOARCH=386 go build -v -o bin/temperature_logger_x32
-echo "Linux 64 bit"
-env GOOS=linux GOARCH=amd64 go build -v -o bin/temperature_logger_x64
+env GOOS=linux GOARCH=386 go build -v -o bin/temperature_logger-linux
+#echo "Linux 64 bit"
+#env GOOS=linux GOARCH=amd64 go build -v -o bin/temperature_logger_x64
+
+
+echo "OSX 32 bit"
+env GOOS=darwin GOARCH=386 go build -v -o bin/temperature_logger-OSX
+#echo "OSX 64 bit"
+#env GOOS=darwin GOARCH=amd64 go build -v -o bin/temperature_logger_x64
 
